866d41208720480b080d5b1bcde608b1830d8215
David Rojo <rojo@rojorevolution.com>
2021-03-16 23:38:16 -0500

    made some small footer adjustments for exterior pages

--- client/src/components/Exterior/Footer.js
+++ client/src/components/Exterior/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
 
     return (
         <footer className="container-fluid">
-            <div className="container py-5">
+            <div className="container py-4">
                 <p className="text-light">This project was created for a Full Stack Web Development Bootcamp. Copyright © 2021 RojoRevolution. All Rights Reserved.</p>
             </div>
         </footer>
