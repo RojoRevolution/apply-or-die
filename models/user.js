b51195a930de6da8422e395cccb19bf717214d27
David Rojo <rojo@rojorevolution.com>
2021-03-17 17:19:10 -0500

    set up user schema

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Valid email addres is required']
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 4
+    },
+
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
