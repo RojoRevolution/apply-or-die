569559d118c161192bb576b4532aed47e5f6b5fe
David Rojo <rojo@rojorevolution.com>
2021-03-17 18:06:26 -0500

    udpated server.js file for auth

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const session = require("express-session");
+const passport = require("./config/passport");
 
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +14,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+app.use(session({ secret: "chimichanga", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 // Add routes, both API and view
 app.use(routes);
 
