6be0192452c8dd191c64d0cee9f1875ddb4f598e
David Rojo <rojo@rojorevolution.com>
2021-03-19 10:08:03 -0500

    updated server file to include additional dependencies

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const session = require("express-session");
 const passport = require("./config/passport");
+const cors = require('cors');
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser")
 
-const mongoose = require("mongoose");
 const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// ============================================
+// Middleware Starts
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}))
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 app.use(session({ secret: "chimichanga", resave: true, saveUninitialized: true }));
+app.use(cookieParser("chimichanga"))
 app.use(passport.initialize());
 app.use(passport.session());
 // Add routes, both API and view
