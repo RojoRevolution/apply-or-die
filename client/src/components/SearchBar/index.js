7e983a7d8f7a6bae376eb1d3caf3c1f734dcb6c5
David Rojo <rojo@rojorevolution.com>
2021-03-23 12:54:25 -0500

    cleaned up search bar component

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,30 +1,6 @@
-import React, { useState } from "react";
-import API from "../../utils/API";
-import { useAtom } from "jotai";
-import { apiTitle, apiLocation } from "../../utils/Atoms"
+import React from "react";
 
 function SearchBar(props) {
-    // const [apiTitle] = useAtom(apiTitle);
-    // const [apiLocation] = useAtom(apiLocation);
-
-    // const [formObject, setFormObject] = useState({})
-
-    // function handleInputChange(event) {
-    //     const { name, value } = event.target;
-    //     setFormObject({ ...formObject, [name]: value })
-    //     console.log(formObject)
-    // };
-
-    // function handleSubmit(event) {
-    //     event.preventDefault();
-    //     API.saveData({
-    //         status: formObject.jobTitle,
-    //         title: formObject.jobLocation
-    //     })
-    //         // .then(history.push("/dashboard"))
-    //         .catch(err => console.log(err))
-    // }
-
     return (
         <React.Fragment>
             <div className="searchInputDiv">
