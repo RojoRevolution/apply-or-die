5e7f2f6b4454ef0adec2eb2d2e9c789320e96e8c
David Rojo <rojo@rojorevolution.com>
2021-03-24 11:08:50 -0500

    removed all console logs from client api routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,7 +8,6 @@ export default {
     return axios.get("/api/logs");
   },
   getStatus: function (status) {
-    console.log("getStatus", status)
     return axios.get("/api/logs/", { params: { status } });
   },
   getOne: function (id) {
@@ -18,24 +17,18 @@ export default {
     return axios.delete("/api/logs/" + id);
   },
   saveData: function (logData) {
-    console.log("Log Data: ", logData)
     return axios.post("/api/logs", logData);
   },
   updateData: function (id, logData) {
-    console.log('id: ', id)
-    console.log('logData: ', logData)
     return axios.put(`/api/logs/${id}`, logData);
   },
   newLog: function (id, dataId) {
-    console.log("User ID: ", id)
-    console.log("Data ID: ", dataId)
     return axios.put("/api/user/newapp/" + id, dataId);
   },
   newNote: function (id, noteData) {
     return axios.put("/api/logs/note/" + id, noteData);
   },
   signUp: function (formData) {
-    console.log("Sign Up: ", formData)
     return axios.post("/api/user/signup", formData);
   },
   logIn: function (formData) {
@@ -44,7 +37,6 @@ export default {
     return axios.post("/api/user/login", formData);
   },
   getUser: function (id) {
-    // console.log("API Route: ", id)
     return axios.get("/api/user/info/" + id);
   },
   logOut: function () {
