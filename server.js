8cfb8624b92e8370c4959c7bc8495e9810102db8
David Rojo <rojo@rojorevolution.com>
2021-03-24 11:06:57 -0500

    added some changed settings that I used when trying to deploy on Namecheap, kept them commented out for now

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser")
+const path = require("path")
 
 
 const routes = require("./routes");
@@ -19,11 +20,18 @@ app.use(cors({
   origin: "http://localhost:3000",
   credentials: true
 }))
+// ==========================
+// FOR LOCAL DEV
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   console.log('===== PRODUCTION ====')
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
 }
+
+// ==========================
+// FOR PRODUCTION
+// app.use(express.static(path.join(__dirname, "client/build")));
+// ==========================
 
 app.use(session({ secret: "chimichanga", resave: true, saveUninitialized: true }));
 app.use(cookieParser("chimichanga"))
@@ -37,8 +45,23 @@ require("./config/passport")(passport);
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
+// ========================
+// FOR PRODUCTION USE
+// try {
+//   // Connect to the MongoDB cluster
+//   mongoose.connect(
+//     MONGODB_URI,
+//     { useNewUrlParser: true, useUnifiedTopology: true },
+//     () => console.log(" Mongoose is connected")
+//   );
+
+// } catch (e) {
+//   console.log("could not connect");
+// }
+// ========================
+// FOR DEV USE
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/applyordie");
+// ========================
 
 // Start the API server
 app.listen(PORT, function () {
