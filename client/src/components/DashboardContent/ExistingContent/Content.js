56b8a5d03563d4eb0ca31dcaa46968c0d479fb28
David Rojo <rojo@rojorevolution.com>
2021-03-25 10:19:10 -0500

    wrong location before - target etc now on a tag

--- client/src/components/DashboardContent/ExistingContent/Content.js
+++ client/src/components/DashboardContent/ExistingContent/Content.js
@@ -11,8 +11,8 @@ function Content(props) {
             <h2>{props.title}</h2>
             <p className="margin-none">{props.company} | {props.location}</p>
             <hr />
-            <label className="form-check-label" htmlFor="checkApplied" target="_blank" rel="noopener noreferrer">Listing URL:</label>
-            <p className="margin-none"><a href={props.listing}>{props.listing || "None available"}</a></p>
+            <label className="form-check-label" htmlFor="checkApplied" >Listing URL:</label>
+            <p className="margin-none"><a href={props.listing} target="_blank" rel="noopener noreferrer">{props.listing || "None available"}</a></p>
             <label className="form-check-label mt-4" htmlFor="checkApplied">Description:</label>
             <p className="margin-none">{props.description || "None Available"} </p>
         </div>
