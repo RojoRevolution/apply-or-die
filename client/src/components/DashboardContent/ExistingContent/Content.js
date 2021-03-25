d90ef92a1e51a07dbd7b52246abcef2ffd23a492
David Rojo <rojo@rojorevolution.com>
2021-03-25 10:19:42 -0500

    same change as main to listing url

--- client/src/components/DashboardContent/ExistingContent/Content.js
+++ client/src/components/DashboardContent/ExistingContent/Content.js
@@ -12,7 +12,7 @@ function Content(props) {
             <p className="margin-none">{props.company} | {props.location}</p>
             <hr />
             <label className="form-check-label" htmlFor="checkApplied">Listing URL:</label>
-            <p className="margin-none"><a href={props.listing}>{props.listing || "None available"}</a></p>
+            <p className="margin-none"><a href={props.listing} target="_blank" rel="noopener noreferrer">{props.listing || "None available"}</a></p>
             <label className="form-check-label mt-4" htmlFor="checkApplied">Description:</label>
             <p className="margin-none">{props.description || "None Available"} </p>
         </div>
